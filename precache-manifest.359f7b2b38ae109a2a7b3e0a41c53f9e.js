d1dc6e2a43bc86b1201592cc75ff14fdac4b0bf4
kranthi34 <kothakranthi@hotmail.com>
2020-03-30 12:34:37 +0530

    Updates

---
+++ precache-manifest.359f7b2b38ae109a2a7b3e0a41c53f9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed70fe40de9c7fd50a51acaac17e26ee",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c975135e8302947e186",
+    "url": "/monsters-rolodex/static/css/main.8407f41e.chunk.css"
+  },
+  {
+    "revision": "9d36cb1448d0cd9bbf34",
+    "url": "/monsters-rolodex/static/js/2.9d52131b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9d52131b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c975135e8302947e186",
+    "url": "/monsters-rolodex/static/js/main.e2da0db3.chunk.js"
+  },
+  {
+    "revision": "ef9f4bf876b43359c128",
+    "url": "/monsters-rolodex/static/js/runtime-main.166aba63.js"
+  }
+]);
